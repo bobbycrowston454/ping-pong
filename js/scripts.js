b1fa5305fa7270a5c866b08e01d994e841fe0f2a
bobbycrowston <bobbycrowston454@gmail.com>
2016-08-19 07:54:53 -0700

    add numbers being able to print onto the screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,32 @@
 //back end
-var counting= [];
 var countBy = 1;
 var countTo;
-var counter = function(counter){
-  for(i = 0; i <= countTo; i += countBy){
-     var countTo = i;
-  } return counter;
+var counters = function(counter){
+  var counting= [];
+  for(i = 1; i <= counter; i ++){
+    counting = counting + i;
+    var newCount = counting.split("");
+    console.log(i);
+  } return newCount;
 }
 
 var customWords = function(customWords){
   if( customWords % 15 === 0){
     customWords = "ping-pong";
   }
-    if( customWords % 3 === 0){
-      customWords = "ping";
-    }
-    if( customWords % 5 === 0){
-      customWords = "pong";
-    }
-    return customWords;
-
+  if( customWords % 3 === 0){
+    customWords = "ping";
+  }
+  if( customWords % 5 === 0){
+    customWords = "pong";
+  }
+  return customWords;
 }
 //user interface
 $(function(){
   $('form').submit(function(event){
     event.preventDefault();
     var numberInput = parseInt($('#numInput').val());
-    $("#results").prepend("<li>" + customWords(numberInput) + "</li>");
+    $("#results").prepend("<li>" + counters(numberInput) + "</li>");
   });
 });
