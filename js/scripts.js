7c1e9777acda5df4c3694cbbcbf5cc72f0b7d7d6
bobbycrowston <bobbycrowston454@gmail.com>
2016-08-19 10:08:50 -0700

    add the corrected numbers function

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,58 @@
 //back end
-var countBy = 1;
-var countTo;
-var counters = function(counter){
-  var counting= [];
-  for(i = 1; i <= counter; i ++){
-    counting = counting + i;
-    var newCount = counting.split("");
-    console.log(i);
-  } return newCount;
+var countBy;
+var i = 0;
+
+function countTo(number) {
+    var arr = [];
+    for (var i = 1; i<=number; i++) {
+        arr.push(i.toString());
+    }
+    return arr.toString();
 }
 
-var customWords = function(customWords){
-  if( customWords % 15 === 0){
-    customWords = "ping-pong";
+var customWords = function(customWord){
+  var counting= [];
+  for(var i = 1; i <= customWord; i++){
+    if(( customWord % 15 === 0) || ( customWord % 5 === 0) ||( customWord % 3 === 0)){
+      return true;
+    } else {
+      return false;
+    }
   }
-  if( customWords % 3 === 0){
-    customWords = "ping";
-  }
-  if( customWords % 5 === 0){
-    customWords = "pong";
-  }
-  return customWords;
+  //
+  // if( customWord % 15 === 0){
+  //   customWord = "ping-pong";
+  // }
+  // if( customWord % 3 === 0){
+  //   customWord = "ping";
+  // }
+  // if( customWord % 5 === 0){
+  //   customWords = "pong";
+  // }
+  // return customWords;
 }
+
+var finalNumbers = function(finalWord){
+  var result;
+  result = customWords(customWord);
+  // result = counters(result);
+  return result;
+}
+
+
+
 //user interface
 $(function(){
   $('form').submit(function(event){
     event.preventDefault();
     var numberInput = parseInt($('#numInput').val());
-    $("#results").prepend("<li>" + counters(numberInput) + "</li>");
+    if(!numberInput){
+      alert("please enter a number");
+    }
+    if(numberInput <= 0){
+      alert("please enter a number greater than 0");
+    }
+
+    $("#results").append("<li>" + countTo(numberInput) + "</li>");
   });
 });
